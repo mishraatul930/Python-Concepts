509026dff5cea28fab295ee53b64e8a69c310606
Atul Mishra <mishraatul930@gmail.com>
2026-01-15 11:21:50 +0530

    Datatypes Concept

---
+++ Datatypes.py
@@ -0,0 +1,7 @@
+list = [1,2, "Atul",[1,2,3], True, "Atul"]
+tupple = (1,2,3,[4,5,6],"Atul","Atul",("Parrot","Tiger"))
+dictonary = {"Key":"value","Atul":29,"Aman":31}
+print(list)
+print(tupple)
+print(dictonary)
+# In Python Everything is an OBJECT
