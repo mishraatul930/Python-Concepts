23f2f7cfe87d0c909112db1e5b924994dbe1fca0
Atul Mishra <mishraatul930@gmail.com>
2026-01-15 11:33:26 +0530

    Operators

---
+++ Calculator.py
@@ -0,0 +1,18 @@
+num1 = 100
+num2 = 15
+
+
+
+
+ans1 = num1+num2
+print(f"The addition of {num1} and {num2} is", ans1)
+ans2 = num1 - num2
+print(f"The substraction of {num1} and {num2} is", ans2)
+ans3 = num1 * num2
+print(f"The multiplication of {num1} and {num2} is", ans3)
+ans4 = num1 / num2
+print(f"The division of {num1} and {num2} is", ans4)
+ans5 = num1 // num2
+print(f"The floor division of {num1} and {num2} is", ans5)
+ans6 = num1 ** num2
+print(f"The exponential of {num1} and {num2} is", ans6)
