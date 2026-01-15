cf8aa1fc809c48c02d979a2cb688e36e5aebbe1a
Atul Mishra <mishraatul930@gmail.com>
2026-01-15 10:58:20 +0530

    Python Basics

---
+++ variables.py
@@ -0,0 +1,9 @@
+# Print statements
+print("Hello Python")
+print('''This is new line 
+      to take care''')
+print("This is a \n" \
+"and caaries to new line")
+
+
+print("Hey",6,7, sep='~', end="009\n")
