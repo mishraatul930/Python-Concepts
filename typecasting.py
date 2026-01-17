beee37c58673eede903aa77f253b5ee8299b34bf
Atul Mishra <mishraatul930@gmail.com>
2026-01-17 10:53:59 +0530

    update user input statement

---
+++ typecasting.py
@@ -0,0 +1,22 @@
+a = "1"
+b = "2"
+# Type casting or conversion
+print(int(a)+int(b)) 
+
+# Without conversion
+print(a+b)
+
+# Implicit type casting
+
+c = 1.9
+d = 8
+
+print(c+d)
+
+# User Input
+
+name = input("Enter your name :")
+print(name)
+
+num = int(input("Enter a number"))
+print(num)
